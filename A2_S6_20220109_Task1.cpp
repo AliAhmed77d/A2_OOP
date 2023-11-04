e650f7cec5050599990400bdfdacbed646e4ee1f
shoka20 <50221706+shoka20@users.noreply.github.com>
2023-11-04 15:16:14 +0200

    Add files via upload

---
+++ A2_S6_20220109_Task1.cpp
@@ -0,0 +1,75 @@
+/*
+File:A2_S6_20220109_1.cpp
+Purpose: To organize the sentence and remove any extras
+Author: Hussam Eldin Hesham Ahmed
+Section: S6
+ID: 20220109
+TA: ........................
+Date: 25 Oct 2023
+*/
+
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    int MAX_SENTENCE_LENGTH = 100;
+
+    char sentence[MAX_SENTENCE_LENGTH];
+
+    char correctedSentence[MAX_SENTENCE_LENGTH];
+
+    char previousChar = ' ';
+
+    int correctedIndex = 0;
+
+    cout << "Enter a sentence (up to 100 characters): ";
+    cin.getline(sentence, MAX_SENTENCE_LENGTH, '.');
+
+    for (int i = 0; sentence[i] != '\0'; i++)
+    {
+
+        if (isspace(sentence[i]) || sentence[i] == '\n')
+        {
+
+            // To remove extra spaces
+            if (previousChar != ' ') {
+
+                correctedSentence[correctedIndex++] = ' ';
+
+            }
+
+        }
+        else
+        {
+
+            if (correctedIndex == 0)
+            {
+
+                // Capitalize the first letter
+                correctedSentence[correctedIndex++] = toupper(sentence[i]);
+
+            }
+            else
+            {
+
+                // Convert the rest of the letters to lowercase
+                correctedSentence[correctedIndex++] = tolower(sentence[i]);
+
+            }
+
+        }
+
+        previousChar = sentence[i];
+
+    }
+
+    correctedSentence[correctedIndex] = '.';
+    correctedSentence[correctedIndex + 1] = '\0';
+
+    cout << "\nCorrected sentence: " << correctedSentence << endl;
+
+    return 0;
+}
---
+++ A2_S6_20220109_Task4.cpp
@@ -0,0 +1,88 @@
+/*
+File:A2_S6_20220109_4.cpp
+Purpose: To know all the prime numbers from 2 to the number
+Author: Hussam Eldin Hesham Ahmed
+Section: S6
+ID: 20220109
+TA: ........................
+Date: 25 Oct 2023
+*/
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+void is_prime(int number)
+{
+
+    vector<bool> isPrime(number + 1, true);
+
+    isPrime[0] = isPrime[1] = false; // To make sure that 0 and 1 are not prime
+
+    for (int j = 2; j * j <= number; j++)// looping through potential prime numbers from 2 to the square root of number
+    {
+
+        if (isPrime[j])// If j is marked as prime
+        {
+
+            // Mark all multiples of j as non-prime
+            for (int i = j * j; i <= number; i += j)
+            {
+
+                isPrime[i] = false;
+
+            }
+
+        }
+
+    }
+
+
+    cout << "\nPrime numbers between 2 and " << number << " are:\n\n( ";
+
+    //output the new list
+    for (int i = 2; i <= number; i++)
+    {
+
+        if (isPrime[i])
+        {
+
+            cout << i << ", ";
+
+        }
+
+    }
+
+    cout<<")"<<endl;
+
+}
+
+int main() {
+
+    int number;
+
+    cout << "Enter a number (n) to find all prime numbers between 2 and n: ";
+    cin >> number;
+
+    if (number >= 2)
+    {
+
+        is_prime(number);
+
+
+    }
+    else
+    {
+
+        cout << "\nPlease enter a number bigger than or equal to 2.\n" << endl;
+        main();
+
+    }
+
+    return 0;
+}
+
