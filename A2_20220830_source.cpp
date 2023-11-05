191d44164b9327da189ea3bb2317416c0130d92f
rabeam2022 <148068281+rabeam2022@users.noreply.github.com>
2023-11-05 21:04:56 +0200

    Add files via upload

---
+++ A2_20220830_source.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+bool BigReal(const string& realNumber ) {
+    BigReal(double realNumber) {
+
+    }
+
+    BigReal::BigReal(
+    const std::string &realNumber) {
+
+    }
+
+    BigReal::BigReal(
+    const BigReal &other) {
+
+    }
+
+    BigReal &BigReal::operator=(const BigReal &other) {
+
+    }
+
+    void BigReal::setNum(const string &realNumber) {
+
+    }
+
+    int BigReal::size() const {
+    }
+
+    int BigReal::sign() const {
+    }
+
+    BigReal BigReal::operator+(const BigReal &other) const {
+
+    }
+
+    BigReal BigReal::operator-(const BigReal &other) const {
+
+    }
+
+    bool BigReal::operator<(const BigReal &anotherReal) const {
+        // Implement the less than operator
+    }
+
+    bool BigReal::operator>(const BigReal &anotherReal) const {
+    }
+
+    bool BigReal::operator==(const BigReal &anotherReal) const {
+    }
+
+    ostream &operator<<(ostream &out, const BigReal &num) {
+
+    }
+    return 0;
+}
