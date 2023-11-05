92ab64ceed8949e516af0fd9ecdb1c2d58c0c355
rabeam2022 <148068281+rabeam2022@users.noreply.github.com>
2023-11-05 18:41:45 +0200

    Add files via upload

--- A2_S16_20220830_Task3.cpp
+++ A2_S16_20220830_Task3.cpp
@@ -1,5 +1,37 @@
-//
-// Created by rabea on 05/11/2023.
-//
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> split(const string& target, const string& delimiter) {
+    vector<string> result;
+    size_t start = 0, end = 0;
 
-#include "headervector.h"
+    while ((end = target.find(delimiter, start)) != string::npos) {
+        result.push_back(target.substr(start, end - start));
+        start = end + delimiter.length();
+    }
+
+    result.push_back(target.substr(start));
+    return result;
+}
+
+int main() {
+    string inputA = "10,20,30";
+    string delimiterA = ",";
+    vector<string> resultA = split(inputA, delimiterA);
+
+    string inputB = "do re mi fa so la ti do";
+    string delimiterB = " ";
+    vector<string> resultB = split(inputB, delimiterB);
+
+
+    for (const string& str : resultA) {
+        cout << str << endl;
+    }
+
+    for (const string& str : resultB) {
+        cout << str << endl;
+    }
+
+    return 0;
+}
