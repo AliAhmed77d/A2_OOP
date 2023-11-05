8bdf259a6bf7ae5d3a37fa434aff9137fba736f6
OO7_Ali <147211028+capitan77o@users.noreply.github.com>
2023-11-05 18:42:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ Author:Ali Ahmed
+ ID:20220701
+ section:S15
+ */
+#include "BigReal.h"
+#include <iostream>
+using namespace std;
+int main() {
+    BigReal n1("11.9000000000000000000000000000000001");
+    BigReal n2("2333333333339.1134322222222292");
+    BigReal n3 = n1 + n2;
+    cout << n3 << endl;
+
+    n3 = n3 + BigReal(0.9);
+    cout << n3 << endl;
+
+    return 0;
+}
+
+
